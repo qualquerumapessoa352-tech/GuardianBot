b67ead123d2b3ca17e3413743ad381986cf3b232
qualquerumapessoa352-tech <qualquerumapessoa352@gmail.com>
2026-08-03 00:16:55 +0100

    Implement logging for automatic member bans
    Added logging for member bans to enhance moderation capabilities.

--- main.py
+++ main.py
@@ -3,33 +3,7 @@ from discord.ext import commands
 import os
 
 from antispam import check_spam
-from logs import log_message_delete, log_message_edit
-
-intents = discord.Intents.all()
-
-bot = commands.Bot(
-    command_prefix="!",
-    intents=intents
-)
-
-
-@bot.event
-async def on_ready():
-    print(f"{bot.user} está online!")
-    print("VERSÃO NOVA DO MIMI LIGADA")
-
-    await bot.change_presence(
-        activity=discord.Game(
-            name="🛡️ Protegendo o servidor"
-        )
-    )
-
-
-@bot.event
-async def on_message(message):
-    await check_spam(bot, message)
-    await bot.process_commands(message)
-
+from logs import log_message_delete, log_message_edit, log_ban
 
 @bot.event
 async def on_message_delete(message):
@@ -41,6 +15,12 @@ async def on_message_edit(before, after):
     await log_message_edit(before, after)
 
 
-TOKEN = os.getenv("TOKEN")
-
-bot.run(TOKEN)
+@bot.event
+async def on_member_ban(guild, user):
+    await log_ban(
+        guild,
+        user,
+        None,
+        "Banimento automático",
+        "🤖 Mimi Security"
+    )
