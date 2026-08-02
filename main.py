6d10b42103350b0efd2863f991ff6eddf5916c74
qualquerumapessoa352-tech <qualquerumapessoa352@gmail.com>
2026-08-02 16:56:15 +0100

    Implement spam check in on_message event
    Added spam checking functionality to on_message event.

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import discord
 from discord.ext import commands
 import os
+from antispam import check_spam
 
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix="!", intents=intents)
+
 
 @bot.event
 async def on_ready():
@@ -12,5 +14,13 @@ async def on_ready():
     await bot.change_presence(
         activity=discord.Game(name="🛡️ Protegendo o servidor")
     )
+
+
+@bot.event
+async def on_message(message):
+    await check_spam(bot, message)
+
+    await bot.process_commands(message)
+
 
 bot.run(os.getenv("TOKEN"))
