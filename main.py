8b997efda28aabeef5e397a19ae8c5a257db5c38
qualquerumapessoa352-tech <qualquerumapessoa352@gmail.com>
2026-08-03 00:18:14 +0100

    Add Discord bot setup and event handlers

--- main.py
+++ main.py
@@ -5,6 +5,35 @@ import os
 from antispam import check_spam
 from logs import log_message_delete, log_message_edit, log_ban
 
+
+intents = discord.Intents.all()
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
+
+
+@bot.event
+async def on_ready():
+    print(f"{bot.user} está online!")
+    print("VERSÃO NOVA DO MIMI LIGADA")
+
+    await bot.change_presence(
+        activity=discord.Game(
+            name="🛡️ Protegendo o servidor"
+        )
+    )
+
+
+@bot.event
+async def on_message(message):
+
+    await check_spam(bot, message)
+
+    await bot.process_commands(message)
+
+
 @bot.event
 async def on_message_delete(message):
     await log_message_delete(message)
@@ -24,3 +53,8 @@ async def on_member_ban(guild, user):
         "Banimento automático",
         "🤖 Mimi Security"
     )
+
+
+TOKEN = os.getenv("TOKEN")
+
+bot.run(TOKEN)
